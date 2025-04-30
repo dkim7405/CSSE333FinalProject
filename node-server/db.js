3a549a552394e08207a24370d1c42fd364309745
esly <cerritem@rose-hulman.edu>
2025-04-29 22:41:22 -0400

    Server successfully connected to database

---
+++ node-server/db.js
@@ -0,0 +1,33 @@
+const sql = require('mssql')
+require("dotenv").config(); 
+
+//configuration for the server
+const config = {
+    server: process.env.DB_SERVER,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    options: {
+        encrypt:false, //mssql doesn't need true only azure does
+        enableArithAbort: true
+    },
+    port: parseInt(process.env.DB_PORT)
+};
+
+//creates a connection pool and exports it as a promise 
+const poolPromise = new sql.ConnectionPool(config)
+.connect()
+.then(pool => {
+    console.log('Connected to database');
+    return pool;
+
+})
+.catch(err => {
+    console.error('Database connection failed: ', err)
+    throw err;
+});
+
+module.exports = {
+    sql,
+    poolPromise
+};
