f867599c502f56aff1c0b315cbe4c6f95763aadd
esly <cerritem@rose-hulman.edu>
2025-04-29 23:52:47 -0400

    fixed server port conflict and confirmed working backend

--- node-server/servers.js
+++ node-server/servers.js
@@ -7,5 +7,36 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is running on "${PORT}`));
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+
+
+app.get("/", (req, res) => {
+    res.send("Backend is working");
+  });
+
+  
+//get all users
+// app.get("/api/users", async(req,res) => {
+//     try{
+//         const pool = await poolPromise;
+//         const result = await pool.request().query("SELECT * FROM User");
+//         console.log(result); 
+
+//         res.status(200).json({
+//             success:true,
+//             empData:result.recordset
+//         });
+
+//     }
+//     catch(error){
+//         console.log(`Error`, error); 
+//         res.status(500).json({
+//             success:false,
+//             empData:result.recordset,
+//             message:"Server error, try again",
+//             error: error.message
+//         });
+//     }
+
+// });
